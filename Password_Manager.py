9d07e01e1a5f0e1ee5428b0d99f9d811189a6361
Rebi Roland Renato <116265475+RRRoland10@users.noreply.github.com>
2024-01-29 23:08:08 +0100

    Add files via upload

---
+++ Password_Manager.py
@@ -0,0 +1,9 @@
+pwd = input("What is the master password? ")
+
+mode = input("Would you like to add a new password or view existing ones (view,)")
+if mode == "view":
+  pass
+elif mode == "add":
+  pass
+else:
+  print()
